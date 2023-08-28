4e3ccd43ca04926a03206fd5f6c5363c37b125d0
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-28 12:47:41 +0330

    init custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=11, null=True, blank=True)
+    job = models.CharField(max_length=150, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
