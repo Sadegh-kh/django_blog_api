e8bc13af4c0e0a2124a3376f2e9c1b659311121e
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-29 10:50:23 +0330

    init post model to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
+
 # Register your models here.
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "created_at"]
+    ordering = ["created_at"]
