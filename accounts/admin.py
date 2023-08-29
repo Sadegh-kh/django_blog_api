84d217f6bd1963dbe60efebf1113ac454dfe860b
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-29 10:17:57 +0330

    init custom user to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from . import forms
+from .models import CustomUser
+
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    form = forms.CustomUserChangeForm
+    add_form = forms.CustomUserCreateForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional infromation",
+            {"fields": ("phone", "job", "bio")},
+        ),
+    )  # type: ignore
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional infromation",
+            {"fields": ["phone", "job", "bio"]},
+        ),
+    )
+
+    list_display = ["username", "first_name", "last_name", "email", "is_staff"]
---
+++ accounts/forms.py
@@ -0,0 +1,20 @@
+from dataclasses import fields
+
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from .models import CustomUser
+
+
+class CustomUserCreateForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = [
+            "username",
+            "email",
+        ]
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "phone", "job", "bio"]
