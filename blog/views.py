553ecb9b37d1e73ccc88e6004c2f05d4025fdfe7
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-08-30 12:11:31 +0330

    init custom permission for list and detail api

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,18 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 
 # Create your views here.
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 
 class PostListApi(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
 
 
 class PostDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAuthorOrReadOnly,)
