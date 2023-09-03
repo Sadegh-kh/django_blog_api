ba6db77361df0a4af638279dc67534879b97fad8
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-09-03 11:41:40 +0330

    init user list and user detail endpoints

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from blog import views
+from . import views
 
 app_name = "blog"
 urlpatterns = [
     path("", views.PostListApi.as_view(), name="post_list"),
     path("<int:pk>/", views.PostDetailApi.as_view(), name="post_detail"),
+    path("users/", views.UserListApi.as_view(), name="user_list"),
+    path("users/<int:pk>", views.UserDetailApi.as_view(), name="user_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
 
 # Create your views here.
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 
 
 class PostListApi(generics.ListCreateAPIView):
@@ -16,3 +17,13 @@ class PostDetailApi(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthorOrReadOnly,)
+
+
+class UserListApi(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailApi(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
