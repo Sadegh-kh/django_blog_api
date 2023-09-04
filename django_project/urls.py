6e1b21f0bd24b3910f668dd02c273d45ea416684
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-09-04 10:46:27 +0330

    add dynamic schema generator

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("apis/v1/", include("blog.urls", namespace="blog")),
+    path("api/v1/", include("blog.urls", namespace="blog")),
     path("api-auth/", include("rest_framework.urls")),
-    path("apis/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
-    path(
-        "apis/v1/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")
-    ),
+    path("api/v1/rest-auth/", include("dj_rest_auth.urls")),
+    path("api/v1/rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
 ]
